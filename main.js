2122b8c8836fc00db0d2ed6da65c69ebbfc39a65
paln8n7634@gmail.com <paln8n7634@gmail.com>
2023-05-27 21:16:06 +0530

    second change

--- main.js
+++ main.js
@@ -5,3 +5,19 @@ change.forEach((change)=>{
     change.classList.toggle("changeColor");
   }
 });
+
+
+var menubtn = document.querySelector(".menubtn");
+var mobNav = document.querySelector(".mob-nav");
+
+mobNav.style.maxWidth = "0px";
+
+menubtn.onclick = function(){
+  if(mobNav.style.maxWidth == "0px"){
+    mobNav.style.maxWidth = "100%";
+  }
+  else{
+    mobNav.style.maxWidth = "0px";
+  }
+}
+
