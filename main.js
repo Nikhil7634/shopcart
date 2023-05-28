dbb852c5426c47366b6a90b647ccd9e706b3ebd2
unknown <paln8n7634@gmail.com>
2023-05-27 18:25:16 -0900

    fist page half complite

--- main.js
+++ main.js
@@ -9,15 +9,9 @@ change.forEach((change)=>{
 
 var menubtn = document.querySelector(".menubtn");
 var mobNav = document.querySelector(".mob-nav");
-
-mobNav.style.maxWidth = "0px";
-
+/*
+mobNav.style.maxWidth = "0px";*/
 menubtn.onclick = function(){
-  if(mobNav.style.maxWidth == "0px"){
-    mobNav.style.maxWidth = "100%";
-  }
-  else{
-    mobNav.style.maxWidth = "0px";
-  }
+  mobNav.classList.toggle("openNav");
 }
 
